a056522d21ea9e5f359edab393a0708d4a78d753
Devon Tuma <dtumad@gmail.com>
2025-06-03 14:42:54 -0700

    finalize files

--- SP1Operations/Add4Operation.lean
+++ SP1Operations/Add4Operation.lean
@@ -1,57 +1,83 @@
 import SP1Foundations
 
-@[reducible] def Add4Operation2 (T : Type) := Word T
+structure Add4Operation where
+  value : Word U16
 
-namespace Add4Operation2
+def Add4Operation.spec
+  (cols : Add4Operation)
+  (a : Word U16)
+  (b : Word U16)
+  (cc : Word U16)
+  (d : Word U16)
+  (is_real : U2) : Prop :=
+    is_real = U2.one →
+    a.toFin32_U16 + b.toFin32_U16 + cc.toFin32_U16 + d.toFin32_U16 = cols.value.toFin32_U16
 
-/-- `AddOperation` should either give the direct sum of the two input values, with one possible overflow.
-This is more explicit than saying that `(a.toNat + b.toNat) % 2^32 = cols.toNat`. -/
-def spec (cols : Add4Operation2 (Fin p))
-    (a b c d : Word (Fin p)) : Prop :=
-  a.isUInt32 → b.isUInt32 → c.isUInt32 → d.isUInt32 →
-    (a.toNat + b.toNat + c.toNat + d.toNat) % 2^32 = cols.toNat
+def Add4Operation.constraints
+    (cols : Add4Operation)
+    (a : Word U16)
+    (b : Word U16)
+    (cc : Word U16)
+    (d : Word U16)
+    (is_real : U2): Prop :=
+    (is_real = U2.one → ((((((a[0].val + b[0].val) + cc[0].val) + d[0].val) - cols.value[0].val) + 0) * 2013235201) < 256) -- checking this is < 8 bytes
+    ∧ (is_real = U2.one → ((((((a[1].val + b[1].val) + cc[1].val) + d[1].val) - cols.value[1].val) + ((((((a[0].val + b[0].val) + cc[0].val) + d[0].val) - cols.value[0].val) + 0) * 2013235201)) * 2013235201) < 256)
 
--- /-- TODO: Constraints on `AddOperation` as extracted from the source code. -/
--- def extractedConstraints (cols : AddOperation (Fin p))
---     (a : Word (Fin p)) (b : Word (Fin p)) : Prop :=
---   (((((a[0] + b[0]) - cols[0]) + (0 : Fin p)) *
---     (2013235201 : Fin p)) * (((((a[0] + b[0]) - cols[0]) + 0) * 2013235201) - 1)) = 0 ∧
---     (((((a[1] + b[1]) - cols[1]) + ((((a[0] + b[0]) - cols[0]) + (0 : Fin p)) *
---     (2013235201 : Fin p))) * (2013235201 : Fin p)) * (((((a[1] + b[1]) - cols[1]) +
---     ((((a[0] + b[0]) - cols[0]) + 0) * 2013235201)) * 2013235201) - 1)) = 0 ∧
---     cols[0].val < base ∧ cols[1].val < base
+def Add4Operation.constraints'
+    (cols : Add4Operation)
+    (a : Word U16)
+    (b : Word U16)
+    (cc : Word U16)
+    (d : Word U16)
+    (is_real : U2): Prop :=
+    let carry0 : BabyBear := 0
+    let carry1 : BabyBear := (a[0].val + b[0].val + cc[0].val + d[0].val - cols.value[0].val + carry0) * baseInv
+    let carry2 : BabyBear := (a[1].val + b[1].val + cc[1].val + d[1] - cols.value[1].val + carry1) * baseInv
+    (is_real = U2.one → carry1 < 256)
+    ∧ (is_real = U2.one → carry2 < 256)
 
-/-- Cleaned up representation of the `AddOperation` constraints. -/
-def idealizedConstraints (cols : Add4Operation2 (Fin p))
-    (aw bw cw dw : Word (Fin p)) : Prop :=
-  let carry0 := 0
-  let carry1 := (aw[0] + bw[0] + cw[0] + dw[0] - cols[0] + carry0) * baseInv
-  let carry2 := (aw[1] + bw[1] + cw[1] + dw[1] - cols[1] + carry1) * baseInv
-  carry1 * (carry1 - 1) = 0 ∧ -- isBool check
-  carry2 * (carry2 - 1) = 0 ∧ -- isBool check
-  cols.isUInt32 -- slice range checks
+theorem helper1 {n : Nat} {a : Nat} {ha : a < n} {x : Nat} : (Fin.mk a ha) < x → a % n < x := by sorry
 
--- /-- TODO: The idealized constraints are logically equivalent to the extracted ones. -/
--- lemma extractedConstraints_iff_idealizedConstraints (cols : AddOperation (Fin p))
---     (a : Word (Fin p)) (b : Word (Fin p)) :
---     cols.extractedConstraints a b ↔ cols.idealizedConstraints a b := by
---   simp [extractedConstraints, idealizedConstraints, Word.isUInt32]
+def Add4Operation.constraints_iff_constraints'
+  (cols : Add4Operation)
+  (a : Word U16)
+  (b : Word U16)
+  (cc : Word U16)
+  (d : Word U16)
+  (is_real : U2)
+  : cols.constraints a b cc d is_real ↔ cols.constraints' a b cc d is_real :=
+  by
+    simp [constraints, constraints']
 
-/-- The extracted constraints on `AddOperation` imply the spec. -/
-theorem correct [Fact (Nat.Prime p)]
-    (cols : Add4Operation2 (Fin p)) (aw bw cw dw: Word (Fin p)) :
-    cols.idealizedConstraints aw bw cw dw → cols.spec aw bw cw dw := by
-  -- Unfold the definitions of constraints and spec
-  simp [idealizedConstraints, spec, sub_eq_zero, mul_eq_zero]
-  -- Introduce all of the hypothesis from the constraints
-  intros h1 h2 hcols_u32 ha_u32 hb_u32
-  -- Split on whether the lower limb addition causes a carry
-  cases h1 with | inl h1 => ?_ | inr h1 => ?_
-  all_goals
-  · rw [h1] at h2
-    -- Reduce expressions to natural number arithmetic
-    simp [Fin.add_def, Fin.sub_def, Fin.ext_iff, p, Word.toNat, Word.isUInt32] at *
-    -- Apply linear constraint solver
-    sorry --omega
+theorem Add4Operation.correct (cols : Add4Operation)
+  (a : Word U16)
+  (b : Word U16)
+  (cc : Word U16)
+  (d : Word U16)
+  (is_real : U2)
+  : cols.constraints a b cc d is_real → cols.spec a b cc d is_real :=
+  by
+    intro ⟨h1, h2⟩
+    intro h_is_real
 
-end Add4Operation2
+    have q1 : ((((((a[0].val + b[0].val) + cc[0].val) + d[0].val) - cols.value[0].val) + 0) * baseInv) < 256 := h1 h_is_real
+    have q2 : ((((((a[1].val + b[1].val) + cc[1].val) + d[1].val) - cols.value[1].val) + ((((((a[0].val + b[0].val) + cc[0].val) + d[0].val) - cols.value[0].val) + 0) * baseInv)) * baseInv) < 256 := h2 h_is_real
+    clear h1 h2
+    simp at q1 q2
+
+    -- Extract bounds on values
+    have a0_bound : a[0].val.val < 65536 := a[0].in_range
+    have a1_bound : a[1].val.val < 65536 := a[1].in_range
+    have b0_bound : b[0].val.val < 65536 := b[0].in_range
+    have b1_bound : b[1].val.val < 65536 := b[1].in_range
+    have c0_bound : cc[0].val.val < 65536 := cc[0].in_range
+    have c1_bound : cc[1].val.val < 65536 := cc[1].in_range
+    have d0_bound : d[0].val.val < 65536 := d[0].in_range
+    have d1_bound : d[1].val.val < 65536 := d[1].in_range
+    have v0_bound : cols.value[0].val.val < 65536 := cols.value[0].in_range
+    have v1_bound : cols.value[1].val.val < 65536 := cols.value[1].in_range
+
+    simp [Word.toFin32_U16]
+    simp [Fin.add_def, Fin.mul_def]
+
+    sorry
