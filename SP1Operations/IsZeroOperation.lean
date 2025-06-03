ec6d7cfacf5e45398c06636c95b0525cc4cbe42c
Devon Tuma <dtumad@gmail.com>
2025-06-03 15:45:02 -0700

    simple is_zero

--- SP1Operations/IsZeroOperation.lean
+++ SP1Operations/IsZeroOperation.lean
@@ -12,23 +12,20 @@ namespace IsZeroOperation
 
 def spec (cols : IsZeroOperation)
     (a : U16) (is_real : U2) : Prop :=
-  is_real = 1 →
-    (a = 0 ↔ cols.result = 1)
+  is_real = 1 → (a = 0 ↔ cols.result = 1)
 
 def constraints (cols : IsZeroOperation)
     (a : U16) (is_real : U2) : Prop :=
-  (is_real.val * ((1 - (cols.inverse.val * a.val)) - cols.result.val)) = (0 : BabyBear)
-  ∧ (is_real.val * (cols.result.val * (cols.result.val - (1 : BabyBear)))) = (0 : BabyBear)
-  ∧ (is_real.val * (cols.result.val * (cols.result.val - (1 : BabyBear)))) = (0 : BabyBear)
-  ∧ (is_real.val * (cols.result.val * a.val)) = (0 : BabyBear)
+  (is_real.val * ((1 - (cols.inverse.val * a.val)) - cols.result.val)) = 0
+  ∧ (is_real.val * (cols.result.val * (cols.result.val - 1))) = 0
+  ∧ (is_real.val * (cols.result.val * (cols.result.val - 1))) = 0
+  ∧ (is_real.val * (cols.result.val * a.val)) = 0
 
 theorem correct (cols : IsZeroOperation)
     (a : U16) (is_real : U2) :
     cols.constraints a is_real → cols.spec a is_real := by
   intro ⟨h1, h2, h3, h4⟩ h_is_real
-  -- Since is_real = 1, substitute and apply 1 * x = x to get original constraints
-  have is_real_val_eq_one : is_real.val = 1 := by aesop
-  simp [is_real_val_eq_one, sub_eq_zero] at *
+  simp [sub_eq_zero] at *
   aesop
 
 end IsZeroOperation
