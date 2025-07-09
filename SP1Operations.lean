4c344071b80b1a365f7258a8bb8c722d528b61a8
Devon Tuma <dtumad@gmail.com>
2025-07-09 14:12:26 -0700

    import missing

--- SP1Operations.lean
+++ SP1Operations.lean
@@ -7,5 +7,6 @@ import SP1Operations.IsZeroOperation
 import SP1Operations.JTypeReader
 import SP1Operations.MulOperation
 import SP1Operations.RTypeReader
+import SP1Operations.U16MSBOperation
 import SP1Operations.U16toU8OperationSafe
 import SP1Operations.U16toU8OperationUnsafe
