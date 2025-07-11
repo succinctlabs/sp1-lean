a82aca5818b5bf2feb1ab0c171f70489ace7f624
Gavin Zhao <gavin@succinct.xyz>
2025-07-11 11:14:10 -0700

    import RV64IM

---
+++ LeanRV64IM/RiscvAddrChecks.lean
@@ -0,0 +1,163 @@
+import LeanRV64IM.RiscvRegs
+
+set_option maxHeartbeats 1_000_000_000
+set_option maxRecDepth 1_000_000
+set_option linter.unusedVariables false
+set_option match.ignoreUnusedAlts true
+
+open Sail
+
+namespace LeanRV64IM.Functions
+
+open zvk_vsm4r_funct6
+open zvk_vsha2_funct6
+open zvk_vaesem_funct6
+open zvk_vaesef_funct6
+open zvk_vaesdm_funct6
+open zvk_vaesdf_funct6
+open zicondop
+open wxfunct6
+open wvxfunct6
+open wvvfunct6
+open wvfunct6
+open wrsop
+open write_kind
+open wmvxfunct6
+open wmvvfunct6
+open vxsgfunct6
+open vxmsfunct6
+open vxmfunct6
+open vxmcfunct6
+open vxfunct6
+open vxcmpfunct6
+open vvmsfunct6
+open vvmfunct6
+open vvmcfunct6
+open vvfunct6
+open vvcmpfunct6
+open vregno
+open vregidx
+open vmlsop
+open vlewidth
+open visgfunct6
+open virtaddr
+open vimsfunct6
+open vimfunct6
+open vimcfunct6
+open vifunct6
+open vicmpfunct6
+open vfwunary0
+open vfunary1
+open vfunary0
+open vfnunary0
+open vextfunct6
+open uop
+open sopw
+open sop
+open seed_opst
+open ropw
+open rop
+open rmvvfunct6
+open rivvfunct6
+open rfvvfunct6
+open regno
+open regidx
+open read_kind
+open pmpAddrMatch
+open physaddr
+open option
+open nxsfunct6
+open nxfunct6
+open nvsfunct6
+open nvfunct6
+open nisfunct6
+open nifunct6
+open mvxmafunct6
+open mvxfunct6
+open mvvmafunct6
+open mvvfunct6
+open mmfunct6
+open maskfunct3
+open iop
+open instruction
+open fwvvmafunct6
+open fwvvfunct6
+open fwvfunct6
+open fwvfmafunct6
+open fwvffunct6
+open fwffunct6
+open fvvmfunct6
+open fvvmafunct6
+open fvvfunct6
+open fvfmfunct6
+open fvfmafunct6
+open fvffunct6
+open extop_zbb
+open extension
+open exception
+open ctl_result
+open csrop
+open cregidx
+open cbop_zicbom
+open bropw_zbb
+open bropw_zba
+open brop_zbs
+open brop_zbkb
+open brop_zbb
+open brop_zba
+open bop
+open biop_zbs
+open barrier_kind
+open amoop
+open agtype
+open WaitReason
+open TrapVectorMode
+open SATPMode
+open Register
+open Privilege
+open PmpAddrMatchType
+open PTW_Error
+open PTE_Check
+open InterruptType
+open Ext_PhysAddr_Check
+open Ext_FetchAddr_Check
+open Ext_DataAddr_Check
+open Ext_ControlAddr_Check
+open ExtStatus
+open ExecutionResult
+open ExceptionType
+open Architecture
+open AccessType
+
+def ext_fetch_check_pc (start_pc : (BitVec 64)) (pc : (BitVec 64)) : (Ext_FetchAddr_Check Unit) :=
+  (Ext_FetchAddr_OK (Virtaddr pc))
+
+def ext_handle_fetch_check_error (err : Unit) : Unit :=
+  ()
+
+def ext_control_check_addr (pc : (BitVec 64)) : (Ext_ControlAddr_Check Unit) :=
+  (Ext_ControlAddr_OK (Virtaddr pc))
+
+def ext_control_check_pc (pc : (BitVec 64)) : (Ext_ControlAddr_Check Unit) :=
+  (Ext_ControlAddr_OK (Virtaddr pc))
+
+def ext_handle_control_check_error (err : Unit) : Unit :=
+  ()
+
+/-- Type quantifiers: width : Nat, 1 ≤ width ∧ width ≤ 4096 -/
+def ext_data_get_addr (base : regidx) (offset : (BitVec 64)) (acc : (AccessType Unit)) (width : Nat) : SailM (Ext_DataAddr_Check Unit) := do
+  let addr ← do (pure (Virtaddr ((← (rX_bits base)) + offset)))
+  (pure (Ext_DataAddr_OK addr))
+
+def ext_handle_data_check_error (err : Unit) : Unit :=
+  ()
+
+/-- Type quantifiers: k_ex64728# : Bool, k_ex64727# : Bool, k_ex64726# : Bool, k_ex64725# : Bool, size
+  : Nat, 0 < size ∧ size ≤ max_mem_access -/
+def ext_check_phys_mem_read (access_type : (AccessType Unit)) (paddr : physaddr) (size : Nat) (acquire : Bool) (release : Bool) (reserved : Bool) (read_meta : Bool) : Ext_PhysAddr_Check :=
+  (Ext_PhysAddr_OK ())
+
+/-- Type quantifiers: size : Nat, 0 < size ∧ size ≤ max_mem_access -/
+def ext_check_phys_mem_write (write_kind : write_kind) (paddr : physaddr) (size : Nat) (data : (BitVec (8 * size))) (metadata : Unit) : Ext_PhysAddr_Check :=
+  (Ext_PhysAddr_OK ())
+
