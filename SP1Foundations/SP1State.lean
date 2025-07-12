81045b71788f87acd83643660306a34f68949188
Devon Tuma <dtumad@gmail.com>
2025-07-11 18:58:03 -0700

    fix add chip

--- SP1Foundations/SP1State.lean
+++ SP1Foundations/SP1State.lean
@@ -1,20 +1,20 @@
 import SP1Foundations.Constraint
 
 /-- State for arithmetic chip verification is a program counter and register assignment map. -/
-abbrev SP1State := BitVec 32 × (regidx → BitVec 32)
+abbrev SP1State := BitVec 64 × (regidx → BitVec 64)
 
 /-- Add `4` to the current program counter state. -/
 @[reducible] def incrementPC : StateM SP1State Unit :=
   do modify (.map (· + (BitVec.ofNat _ 4)) id)
 
-@[reducible] def setPC (new_pc : BitVec 32) : StateM SP1State Unit :=
+@[reducible] def setPC (new_pc : BitVec 64) : StateM SP1State Unit :=
   do modify (.map (fun _old_pc => new_pc) id)
 
 /-- Modify the register map state -/
-@[reducible] def update_reg (idx : regidx) (v : BitVec 32) : StateM SP1State Unit :=
+@[reducible] def update_reg (idx : regidx) (v : BitVec 64) : StateM SP1State Unit :=
   do modify (.map id (Function.update · idx v))
 
-@[reducible] def get_reg (idx : regidx) : StateM SP1State (BitVec 32) :=
+@[reducible] def get_reg (idx : regidx) : StateM SP1State (BitVec 64) :=
   do return (← get).2 idx
 
 -- dt: below isn't enough to handle read and write both
