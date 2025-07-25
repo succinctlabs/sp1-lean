c45f0007f5a9deb64e5a693ec693491e3f0f377e
Gavin Zhao <gavin@succinct.xyz>
2025-07-25 12:00:57 -0700

    correct JAL/j_type

--- SP1Foundations/Opcode.lean
+++ SP1Foundations/Opcode.lean
@@ -117,8 +117,9 @@ def trusted_instr
       ∧ op_b_0 >= 2^12
       ∧ BitVec.signExtend 64 (BitVec.ofNat 32 (op_b_0.val + op_b_1.val * 65536)) = Word.toBitVec64 #v[op_b_0, op_b_1, op_b_2, op_b_3]
   | JAL =>
-      (op_b_0 < 32 ∧ op_b_1 = 0 ∧ op_b_2 = 0 ∧ op_b_3 = 0)
-      -- 2^12 = 4096
+      -- j_type
+      (imm_b = 0 ∧ imm_c = 1)
+      ∧ (op_b_0 < 32 ∧ op_b_1 = 0 ∧ op_b_2 = 0 ∧ op_b_3 = 0)
       ∧ Word.toBitVec64 #v[op_c_0, op_c_1, op_c_2, op_c_3] = BitVec.signExtend 64 (BitVec.ofNat 21 (op_c_0.val + op_c_1.val * 65536))
   | LB | LH | LW | LD | LBU | LHU | LWU =>
       i_type_constraints op_a op_b_0 op_b_1 op_b_2 op_b_3 op_c_0 op_c_1 op_c_2 op_c_3 imm_b imm_c
