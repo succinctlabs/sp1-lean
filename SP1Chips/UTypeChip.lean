4a976544a9dde16a94272c3be228bdcddb783a83
Devon Tuma <dtumad@gmail.com>
2025-07-25 18:20:51 -0700

    feat: UType chip

--- SP1Chips/UTypeChip.lean
+++ SP1Chips/UTypeChip.lean
@@ -1,1 +1,18 @@
 import SP1Chips.UType.Constraints
+
+
+namespace UTypeChip
+
+open BitVec
+
+open Sail SailState BitVec LeanRV64IM.Functions
+
+variable (Main : Vector (Fin BB) 31) (s : SailState)
+
+def spec_utype (imm : (BitVec 20)) (rd : regidx) (op : uop) : SailM Unit := do
+  Sail.writeReg Register.nextPC ((← Sail.readReg Register.PC) + 4#64)
+  let _ ← execute_UTYPE imm rd op
+
+#check execute_UTYPE
+
+end UTypeChip
