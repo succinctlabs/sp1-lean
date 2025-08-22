3d46b8400af1cde0c5c58ef9a496a213dd00291b
Devon Tuma <dtumad@gmail.com>
2025-08-22 14:58:24 -0700

    CI check

--- SP1Chips/BranchChip.lean
+++ SP1Chips/BranchChip.lean
@@ -1,6 +1,6 @@
 -- These are broken temporarily until chaning `simpM` stuff Gavin was using
 
--- import SP1Chips.Branch.BEQ
+import SP1Chips.Branch.BEQ
 -- import SP1Chips.Branch.BNE
 -- import SP1Chips.Branch.BGE
 -- import SP1Chips.Branch.BLT
