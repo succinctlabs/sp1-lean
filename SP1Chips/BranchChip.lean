03af94fc6898fb99d8702a3e2ba6d293a5baed41
Devon Tuma <dtumad@gmail.com>
2025-08-29 13:42:14 -0700

    temp fix

--- SP1Chips/BranchChip.lean
+++ SP1Chips/BranchChip.lean
@@ -2,7 +2,7 @@
 
 import SP1Chips.Branch.BEQ
 import SP1Chips.Branch.BNE
--- import SP1Chips.Branch.BGE
+import SP1Chips.Branch.BGE
 -- import SP1Chips.Branch.BLT
 import SP1Chips.Branch.BLTU
 -- import SP1Chips.Branch.BGEU
