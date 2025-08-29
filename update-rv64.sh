683df68bd32da6b83abd29cce1e58ede6ed1a22c
Devon Tuma <dtumad@gmail.com>
2025-08-28 17:12:30 -0700

    update scripting

--- update-rv64.sh
+++ update-rv64.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/opencompl/sail-riscv-lean
+git clone --branch sp1-lean-air-verification https://github.com/succinctlabs/sail-riscv-lean
 
 rm -rf LeanRV64D LeanRV64D.lean
 cp -r sail-riscv-lean/LeanRV64D ./LeanRV64D
 cp sail-riscv-lean/LeanRV64D.lean ./LeanRV64D.lean
 rm -rf sail-riscv-lean
-
-sed -i '' 's/let rs1_int/let rs1_int : Int/g' LeanRV64D/RiscvInstsEnd.lean
-sed -i '' 's/let rs2_int/let rs2_int : Int/g' LeanRV64D/RiscvInstsEnd.lean
